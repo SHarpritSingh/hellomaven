3b4a045a13b5e5ab25d04574da06a201df3abba5
harprit singh <harprit.singh@itaas.com>
2013-02-04 18:35:27 +0530

    initial commit

---
+++ src/main/java/com/itaas/App.java
@@ -0,0 +1,13 @@
+package com.itaas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
